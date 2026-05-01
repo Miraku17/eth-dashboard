3d221ccf6cd992057b9f57cd377c14d42c2ed8ea
Miraku17 <zhaztedv@gmail.com>
2026-05-01 13:49:59 +0800

    feat(layout): add Markets / Onchain / Mempool routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthGate from "./components/AuthGate";
 import DashboardShell from "./components/DashboardShell";
 import WalletDrawer from "./components/WalletDrawer";
+import MarketsPage from "./routes/MarketsPage";
+import MempoolPage from "./routes/MempoolPage";
+import OnchainPage from "./routes/OnchainPage";
 import OverviewPage from "./routes/OverviewPage";
 
 export default function App() {
@@ -12,6 +15,9 @@ export default function App() {
         <Routes>
           <Route element={<DashboardShell />}>
             <Route index element={<OverviewPage />} />
+            <Route path="markets" element={<MarketsPage />} />
+            <Route path="onchain" element={<OnchainPage />} />
+            <Route path="mempool" element={<MempoolPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
